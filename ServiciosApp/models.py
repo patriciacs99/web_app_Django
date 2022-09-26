2068871883962d38b58c84c22d4ef73c6f4cad34
Patricia <patriciacorralsanz@gmail.com>
2022-09-26 15:09:46 +0200

    image control added

--- ServiciosApp/models.py
+++ ServiciosApp/models.py
@@ -9,7 +9,7 @@ class Servicio(models.Model):
 
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='servicios')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
