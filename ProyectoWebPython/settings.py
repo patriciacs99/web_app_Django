fd05f3e97ebd0eaf7bbaf1fa71c53e8c53d7a6fa
Patricia <patriciacorralsanz@gmail.com>
2022-09-26 19:56:27 +0200

    Blog added

--- ProyectoWebPython/settings.py
+++ ProyectoWebPython/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ProyectoWebApp',
     'ServiciosApp',
+    'BlogApp',
 ]
 
 MIDDLEWARE = [
--- ProyectoWebPython/urls.py
+++ ProyectoWebPython/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('ProyectoWebApp.urls'))
-    
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('',include('ProyectoWebApp.urls')),
+    path('servicios/',include('ServiciosApp.urls')),
+    path('blog/',include('BlogApp.urls')),
+]
